5a72cf0e3ba42c2858af821111b23b6e4cf35a2f
Haitam <ennouskihaitam@gmail.com>
2025-10-03 16:21:56 +0100

    start the admin backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const userRouter = require('./routes/userRoutes');
+const adminRouter = require('./routes/adminRoutes');
 
 app.use(cors());
 app.use(express.json()); // Parse Json Request Body
 
 
 app.use('/',userRouter);
+app.use('/admin',adminRouter);
 
 
 module.exports = app;
