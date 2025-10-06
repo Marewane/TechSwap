1afc1e5c5fd071e29ee2744cb38159337343013a
Haitam <ennouskihaitam@gmail.com>
2025-10-06 11:17:51 +0100

    solve some merge conflcits

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const paymentRoutes = require("./routes/paymentRoutes");
 const userRoutes = require('./routes/userRoutes');
-const adminRouter = require('./routes/adminRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 const cors = require("cors");
 
 const app = express();
 const cors = require('cors');
-const userRouter = require('./routes/userRoutes');
 
 app.use(cors());
 app.use(express.json()); // Parse Json Request Body
 
 // Register payment routes
 app.use("/api/payments", paymentRoutes);
-app.use('/',userRouter);
-app.use('/admin',adminRouter);
+app.use('/',userRoutes);
+app.use('/admin',adminRoutes);
 
 module.exports = app;
