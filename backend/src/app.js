a308073ee95d3710715f53e52f5c641ac6d8bfaa
Marewane <laamirimarwane6@gmail.com>
2025-10-07 11:53:16 +0100

    add: stripe checkout which make user able to buy how many coins he wants

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const paymentRoutes = require("./routes/paymentRoutes");
 const userRoutes = require('./routes/userRoutes');
+const webHookRouters = require("./routes/webhooksRouter");
 const cors = require("cors");
 
 const app = express();
+// we use this above of express.json to not convert it to json because the data should be as it is came from stripe for checking signature
+app.use('/api/stripe',webHookRouters);
+
+
 app.use(express.json());
 app.use(cors());
 
--- backend/src/routes/paymentRoutes.js
+++ backend/src/routes/paymentRoutes.js
@@ -1,47 +1,8 @@
 const express = require("express");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
+const {createPaymentIntent} = require('../controllers/payments/paymentController');
 const router = express.Router();
 
-router.post("/create-checkout-session", async (req, res) => {
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment",
-      line_items: [
-        {
-        price_data: {
-            currency: "usd",
-            product_data: { name: "TechSwap Premium Plan" },
-            unit_amount: 1000, // $10
-        },
-        quantity: 1,
-        },
-        {
-        price_data: {
-            currency: "usd",
-            product_data: { name: "Extra Storage (50GB)" },
-            unit_amount: 500, // $5
-        },
-        quantity: 2, // buying 2 of them
-        },
-        {
-        price_data: {
-            currency: "usd",
-            product_data: { name: "One-Time Setup Fee" },
-            unit_amount: 2000, // $20
-        },
-        quantity: 1,
-        },
-        ],
-      success_url: "http://localhost:5500/frontend/success.html",
-      cancel_url: "http://localhost:5500/frontend/cancel.html",
-    });
-
-    res.json({ id: session.id });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
+router.post("/coins/purchase", createPaymentIntent);
+// router.post("/payments/coins/purchase",coinPurchaseController);
 module.exports = router;
