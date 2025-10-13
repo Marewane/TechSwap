90af014978fcb74964a9206ee2aa6eec1c6dbe83
OumniaBASSI <oumniabassi@gmail.com>
2025-10-13 18:26:30 +0100

    add the route for the Users page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminLayout from "./pages/Admin/AdminLayout";
 import Dashboard from "./pages/admin/Dashboard/Dashboard";
+import Users from "./pages/Admin/Users/Users";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         {/* Admin Routes with Sidebar */}
         <Route path="/admin" element={<AdminLayout />}>
           <Route path="dashboard" element={<Dashboard />} />
+          <Route path="users" element={<Users />} />
         </Route>
         
         {/* Other routes */}
